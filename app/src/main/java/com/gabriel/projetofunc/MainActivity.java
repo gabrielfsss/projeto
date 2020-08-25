b301755f81693538e20ca0ba828bb34f88a5d46b
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-08-25 13:36:00 -0600

    trocaTela

--- app/src/main/java/com/gabriel/projetofunc/MainActivity.java
+++ app/src/main/java/com/gabriel/projetofunc/MainActivity.java
@@ -2,39 +2,28 @@ package com.gabriel.projetofunc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button proxTela;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        proxTela = (Button) findViewById(R.id.proxTela); //Criar uma variável para o Button criado no XML
 
-    //so enviar o negocio aquiaaaaaaaasdsdssa
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        proxTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, proxTela.class); // Dentro dos parenteses coloca o nome da activity que você está e a activity que você quer ir, respectivamente
+                startActivity(i); // Iniciar activity
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/gabriel/projetofunc/proxTela.java
@@ -0,0 +1,14 @@
+package com.gabriel.projetofunc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class proxTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prox_tela);
+    }
+}
