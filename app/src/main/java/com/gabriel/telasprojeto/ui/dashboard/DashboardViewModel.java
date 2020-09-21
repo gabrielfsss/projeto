fa04a3d7db284560852606a81d669f1cee5f16d9
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-09-21 03:39:36 -0600

    telas do App aberto

--- app/src/main/java/com/gabriel/telasprojeto/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/gabriel/telasprojeto/ui/dashboard/DashboardViewModel.java
@@ -7,12 +7,6 @@ import androidx.lifecycle.ViewModel;
 public class DashboardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
-
-    public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
-    }
-
     public LiveData<String> getText() {
         return mText;
     }
--- app/src/main/java/com/gabriel/telasprojeto/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/gabriel/telasprojeto/ui/notifications/NotificationsViewModel.java
@@ -7,12 +7,6 @@ import androidx.lifecycle.ViewModel;
 public class NotificationsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
-
-    public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
-    }
-
     public LiveData<String> getText() {
         return mText;
     }
